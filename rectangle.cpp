8fbac94d84f2d92118659802859a88702718f36a
kacperrobox-ai <kacperrobox@gmail.com>
2025-11-17 08:26:05 +0000

    gyufu

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,40 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-class Rectangle {
-
+class Rectangle{
+    private:
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h)
+        : width(w), height(h){}
+    void setWight(double w){
+        width = w;
+    }
+    void setHeight(double h){
+        height = h;
+    }
+    double getWidth()const{
+        return width;
+    }
+    double getHeight()const{
+        return height;
+    }
+    double area()const{
+        return width*height;
+    }
+    double perimeter()const{
+        return 2*(width * height);
+    }
+    void display()const{
+        cout<<"Rectangle:"<< width<<" x "<<height<< endl ;
+        cout<<"Area:"<<area()<<endl;
+        cout<<"Perimeter:"<< perimeter()<<endl;
+    }
+    
 };
+
+
+int main(){
+    return 0;
+}
