1d6a306a51d94c2fa9319148a64f8d5c0e684cb1
kacperrobox-ai <kacperrobox@gmail.com>
2025-12-10 22:28:44 +0000

    67

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,40 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-class Rectangle{
+   class Rectangle {
     private:
     double width;
     double height;
-public:
-    Rectangle(double w, double h)
-        : width(w), height(h){}
-    void setWidth(double w){
+ 
+    public:
+    Rectangle (double w, double h): width(w), height(h) {}
+       void setWidth(double w){
         width = w;
-    }
-    void setHeight(double h){
+       }
+       void setHeight(double h){
         height = h;
-    }
-    double getWidth()const{
-        return width;
-    }
-    double getHeight()const{
-        return height;
-    }
-    double area()const{
-        return width*height;
-    }
-    double perimeter()const{
-        return 2*(width * height);
-    }
-    void display()const{
-        cout<<"Rectangle:"<< width<<" x "<<height<< endl ;
-        cout<<"Area:"<<area()<<endl;
-        cout<<"Perimeter:"<< perimeter()<<endl;
-    }
-    
-};
-
-
-int main(){
-    return 0;
-}
+       }
+         double getWidth() const {
+          return width;
+         }
+            double getHeight() const {
+            return height;
+            }
+            double area() const {
+            return width * height;
+            }
+            double perimeter() const {
+            return 2 * (width + height);
+            }
+            void display() const {
+            cout << "Width: " << width << ", Height: " << height << endl;
+            cout << "Area: " << area() << endl;
+            cout << "Perimeter: " << perimeter() << endl;
+            }
+            };
--- test.cpp
+++ test.cpp
@@ -1,46 +1,29 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch_amalgamated.hpp"
 #include "rectangle.cpp"
-
-TEST_CASE("Rectangle constructor initializes width and height", "[constructor]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE(rect.getWidth() == 10.0);
-    REQUIRE(rect.getHeight() == 5.0);
-}
-
-TEST_CASE("Rectangle setWidth method sets the width", "[setWidth]") {
-    Rectangle rect(10.0, 5.0);
-    rect.setWidth(15.0);
-    REQUIRE(rect.getWidth() == 15.0);
-}
-
-TEST_CASE("Rectangle setHeight method sets the height", "[setHeight]") {
-    Rectangle rect(10.0, 5.0);
-    rect.setHeight(8.0);
-    REQUIRE(rect.getHeight() == 8.0);
-}
-
-TEST_CASE("Rectangle getWidth method returns the width", "[getWidth]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE(rect.getWidth() == 10.0);
-}
-
-TEST_CASE("Rectangle getHeight method returns the height", "[getHeight]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE(rect.getHeight() == 5.0);
-}
-
-TEST_CASE("Rectangle area method calculates the area", "[area]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE(rect.area() == 50.0);
-}
-
-TEST_CASE("Rectangle perimeter method calculates the perimeter", "[perimeter]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE(rect.perimeter() == 30.0);
-}
-
-TEST_CASE("Rectangle display method does not throw an exception", "[display]") {
-    Rectangle rect(10.0, 5.0);
-    REQUIRE_NOTHROW(rect.display());
+ 
+int main() {
+    Rectangle rect(10, 5);
+ 
+    rect.display();
+ 
+    double width = rect.getWidth();
+    double height = rect.getHeight();
+    std::cout << "Width: " << width << ", Height: " << height << "\n";
+ 
+    double area = rect.area();
+    double perimeter = rect.perimeter();
+    std::cout << "Area: " << area << ", Perimeter: " << perimeter << "\n";
+ 
+    rect.setWidth(15);
+    rect.setHeight(8);
+    rect.display();
+ 
+    width = rect.getWidth();
+    height = rect.getHeight();
+    std::cout << "New Width: " << width << ", New Height: " << height << "\n";
+ 
+    area = rect.area();
+    perimeter = rect.perimeter();
+    std::cout << "New Area: " << area << ", New Perimeter: " << perimeter << "\n";
+ 
+    return 0;
 }
